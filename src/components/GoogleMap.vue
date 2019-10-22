fe9b7a02fe7a094d6b8531a5f10ca0f1ad31f0e8
Keisuke Tokuhira <e175722@ie.u-ryukyu.ac.jp>
2019-10-22 13:57:39 +0900

    経路表示機能追加

--- src/components/GoogleMap.vue
+++ src/components/GoogleMap.vue
@@ -6,16 +6,19 @@
           @place_changed="setPlace">
         </gmap-autocomplete>
         <button @click="addMarker">Add</button>
+        <button @click="getDriveplanToDestination">serch</button>
       </label>
-      <br/>
+
 
     </div>
-    <br>
+
     <gmap-map
       :center="center"
       :zoom="15"
       style="width:100%;  height: 500px;"
     >
+    <gmap-polyline v-bind:path.sync="path" v-bind:options="{ strokeColor:'#008000'}"></gmap-polyline>
+
       <gmap-marker
         :key="index"
         v-for="(m, index) in markers"
@@ -27,16 +30,20 @@
 </template>
 
 <script>
+var path = [];
+
 export default {
   name: "GoogleMap",
   data() {
     return {
       // default to Montreal to keep it simple
       // change this to whatever makes sense
+
       center: { lat: 26.231408, lng: 127.685525 },
       markers: [],
       places: [],
-      currentPlace: null
+      currentPlace: null,
+      path: path,
     };
   },
   mounted() {
@@ -66,8 +73,30 @@ export default {
           lng: position.coords.longitude
         };
       });
-    }
+    },
+
+    getDriveplanToDestination(){
+       var result = new Promise(function(resolve) {
+         var req = new XMLHttpRequest();
+         req.open("GET", "http://10.0.0.247:8989/route?point=26.21360535,127.68046814&point=26.24261661,127.75863647&points_encoded=false&instructions=false&debug=true", false); // HTTPメソッドとアクセスするサーバーの　URL　を指定
+         req.send();
+         var rst = JSON.parse(req.responseText).paths[0].points.coordinates;
+         console.log(rst);
+         resolve(rst);
+       });
+
+
+      result.then(function(road2){
+        for(var i=0; i<road2.length; i++){ //ここで入れる
+          var Lat = parseFloat(road2[i][1]);
+          var Lng = parseFloat(road2[i][0]);
+          path.push({lat:Lat,lng:Lng});
+        }
+      });
+
+    },
   }
+
 };
 </script>
 <style scoped>
